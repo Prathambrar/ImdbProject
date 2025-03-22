08509f5117f8bab3d153fa759386dc21a8d6ef72
Prathambrar <prathamjeetsingh@academic.rrc.ca>
2025-03-21 19:17:46 -0500

    adding data from faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,20 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+require 'csv'
+require 'faker'
 
+csv_books_path = Rails.root.join('db', 'seeds', '../../datasets/imdb_movies.csv')
+row_count = 0
+
+CSV.foreach(csv_books_path, headers: true) do |row|
+  break if row_count >= 200
+
+  release_date = begin
+    Date.strptime(row['release_date'], '%m/%d/%Y')
+  rescue ArgumentError
+    Faker::Date.between(from: '1900-01-01', to: Date.today)
+  end
 
   Movie.create!(
     title: row['title'],
@@ -47,12 +60,7 @@ puts "Movies imported successfully! Total: #{row_count} movies."
   end
   
   puts "200 directors have been created successfully!"
-  
-  
-  
-  
-  
-  
+
 require 'faker'
 
 movie_ids = Movie.pluck(:id)

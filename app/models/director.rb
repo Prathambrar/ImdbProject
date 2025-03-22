5357323b44e5834ffeeb1aad63f47b7b2f5d691c
Prathambrar <prathamjeetsingh@academic.rrc.ca>
2025-03-21 19:13:33 -0500

    adding validations in ar models

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,6 +2,6 @@ class Director < ApplicationRecord
     has_many :movie_directors, dependent: :destroy
     has_many :movies, through: :movie_directors
   
-    validates :name, presence: true
+  
   end
   
